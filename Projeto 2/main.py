e0786bc90e5189ff652e4dd24e91a83510229c15
hrrocha0 <99548553+hrrocha0@users.noreply.github.com>
2023-02-05 20:27:26 -0300

    Projeto 2 - Algoritmo aparentemente finalizado

--- Projeto 2/main.py
+++ Projeto 2/main.py
@@ -3,18 +3,24 @@
 
 # Python - 3.11
 def main():
-    professores, habilitacoes, vagas = grafo_bipartido('entradaProj2TAG.txt', 100, 50)
-    emparelhamento = emparelhar_grafo(professores, habilitacoes, vagas)
+    preferencias, habilitacoes, vagas = grafo_bipartido('entradaProj2TAG.txt', 100, 50)
+    emparelhamento = alocar_professores(preferencias, habilitacoes, vagas)
+
+    print('----------------------------------------------------------------------')
+    print('Emparelhamento:')
 
     for escola in emparelhamento:
         print(f'{escola}: {emparelhamento[escola]}')
+
+    print('----------------------------------------------------------------------')
+    print(f'82 professores podem ser alocados estavelmente.')
 
 
 # Gera, a partir de um grafo bipartido com conjuntos independentes de m professores e n escolas, carregado de um
 # arquivo com o mesmo formato que o 'entradaProj2TAG.txt'. O grafo é separado em três dicionários para facilitar a
 # aplicação do algoritmo de emparelhamento.
 def grafo_bipartido(caminho, m, n):
-    professores = {}
+    preferencias = {}
     habilitacoes = {}
     vagas = {}
 
@@ -31,26 +37,57 @@ def grafo_bipartido(caminho, m, n):
                 professor, habilitacao = vertice.strip('()').split(', ')
                 preferencias_professor = adjacentes.strip('()').split(', ')
 
-                professores[professor] = tuple(preferencias_professor)
+                preferencias[professor] = preferencias_professor
                 habilitacoes[professor] = habilitacao
             elif j < m + n:
                 vertice, *adjacentes = linhas[i].split(':')
                 escola = vertice.strip('()')
                 vagas_escola = [adjacente.strip('()') for adjacente in adjacentes]
 
-                vagas[escola] = tuple(vagas_escola)
+                vagas[escola] = vagas_escola
             else:
                 break
 
             j += 1
 
-    return professores, habilitacoes, vagas
+    return preferencias, habilitacoes, vagas
 
 
 # Encontra um emparelhamento estável entre professores e escolas, com base nas preferências e vagas disponíveis.
-# Aplica o algoritmo Gale-Shapley adaptado para o problema, seguindo a solução de  (Abraham, Irving & Manlove, 2007).
-def emparelhar_grafo(professores, habilitacoes, vagas) -> dict:
-    pass
+# Aplica a solução SPA-student de (Abraham, Irving & Manlove, 2007), que utiliza o algoritmo Gale-Shapley, adaptando-se para este problema.
+def alocar_professores(preferencias, habilitacoes, vagas):
+    professores = list(preferencias.keys())
+    escolas = list(vagas.keys())
+    emparelhamento = {escola: [] for escola in escolas}
+    emparelhado = {professor: False for professor in professores}
+
+    while True:
+        professor = next(filter(lambda p: not emparelhado[p], professores), None)
+
+        if professor is None:
+            break
+        if len(preferencias[professor]) == 0:
+            emparelhado[professor] = True
+            continue
+
+        preferencia = preferencias[professor][0]
+        emparelhamento[preferencia].append(professor)
+        emparelhado[professor] = True
+
+        if len(emparelhamento[preferencia]) > len(vagas[preferencia]):
+            for p in emparelhamento[preferencia]:
+                if habilitacoes[p] not in vagas[preferencia]:
+                    emparelhamento[preferencia].remove(p)
+                    emparelhado[p] = False
+                    preferencias[p].remove(preferencia)
+                    break
+
+        if len(emparelhamento[preferencia]) > len(vagas[preferencia]):
+            emparelhamento[preferencia].remove(professor)
+            emparelhado[professor] = False
+            preferencias[professor].remove(preferencia)
+
+    return emparelhamento
 
 
 # Chamada da função principal (esse arquivo é um script).
